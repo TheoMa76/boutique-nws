e296c8e5f8defd60737fa043ff12729c973a2a7c
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-10-29 11:44:10 +0100

    fichier de creation base de donnee + table

---
+++ configs/mig_db/db-29-10-2023.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS boutiquenws;
+USE boutiquenws;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    createdAt DATE,
+    lastLogin DATE
+);
+
+CREATE TABLE produit (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    shortDesc VARCHAR(250) NOT NULL,
+    description TEXT,
+    prix FLOAT NOT NULL,
+    quantite INT NOT NULL,
+    enAvant BOOL NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE commandes (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    adresse TEXT NOT NULL,
+    telephone VARCHAR(20)
+);
+
+CREATE TABLE commandes_produit (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    commande_id INT,
+    produit_id INT,
+    FOREIGN KEY (commande_id) REFERENCES Commandes(id),
+    FOREIGN KEY (produit_id) REFERENCES Produit(id)
+);
